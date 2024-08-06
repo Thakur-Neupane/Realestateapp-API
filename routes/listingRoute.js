8e176370d0a309cf2cd20455ac4ebcf8ade1a833
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-06 21:39:39 +1000

    Update listing route

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -2,10 +2,14 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  getListing,
+  updateListing,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
 
 export default router;
