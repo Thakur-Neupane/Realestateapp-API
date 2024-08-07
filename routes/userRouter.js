042c29bfbf32624dd1934ebac1df1d676a0624b0
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-07 10:52:48 +1000

    Get user by id api

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUser,
   getUserListings,
   updateUser,
 } from "../controllers/userController.js";
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
